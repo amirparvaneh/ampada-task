17b949b812d0dd199b073812404689111db40f10
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-12 23:37:53 +0330

    dev-v1

--- src/main/java/com/ampada/newsapp/controller/UserController.java
+++ src/main/java/com/ampada/newsapp/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.ampada.newsapp.controller;
 
+import com.ampada.newsapp.service.userService.UserServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
+
+    private final UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userService, UserServiceImpl userService1){
+        this.userService = userService;
+    }
 }
--- src/main/java/com/ampada/newsapp/service/boardService/BoardService.java
+++ src/main/java/com/ampada/newsapp/service/boardService/BoardService.java
@@ -3,7 +3,6 @@ package com.ampada.newsapp.service.boardService;
 import com.ampada.newsapp.model.Board;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface BoardService {
     Board createBoard(Board board);
 }

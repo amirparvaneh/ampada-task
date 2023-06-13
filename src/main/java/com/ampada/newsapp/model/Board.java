ef2927aa951bf779edb5ae3a8115d7e9fefbb94a
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-14 00:23:53 +0330

    dev-v1

--- src/main/java/com/ampada/newsapp/model/Board.java
+++ src/main/java/com/ampada/newsapp/model/Board.java
@@ -1,25 +1,17 @@
 package com.ampada.newsapp.model;
-
 import lombok.Data;
-import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
+@Document(collection = "boards")
 @Data
-@Accessors(chain = true)
-@Document(collection = "Board")
 public class Board {
-
     @Id
     private String id;
     private String boardName;
-    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
-    private Date createdOn;
-    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
-    private Date modifiedOn;
-    private User creatorId;
-
+    private LocalDateTime createdOn;
+    private LocalDateTime modifiedOn;
+    private String creatorId;
 }

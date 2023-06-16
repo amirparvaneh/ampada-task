7e96a52611b14385c49b10f38aace234a4c6938f
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-16 10:32:04 +0330

    dev-v1

--- src/main/java/com/ampada/newsapp/config/SwaggerConfig.java
+++ src/main/java/com/ampada/newsapp/config/SwaggerConfig.java
@@ -13,21 +13,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ampada.newsapp.config"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
     }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Task Management System API Documentation")
-                .description("APIs for a task management system like Jira or Trello")
-                .version("1.0.0")
-                .build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.ampada.newsapp.controller"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Task Management System API Documentation")
+//                .description("APIs for a task management system like Jira or Trello")
+//                .version("1.0.0")
+//                .build();
+//    }
 }
